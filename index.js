7bbada2c52420c201ea56bc116ac7fd22334663f
Andrew Emad <andrewemad@Andrews-MacBook-Pro.local>
2026-02-12 07:31:23 +0200

    feat: add routes

--- index.js
+++ index.js
@@ -7,55 +7,82 @@ const path = require('path');
 const server = http.createServer(async(req, res) => {
   const {method, url } = req;
   const params = url.split("/");
-  let status = 200;
+
   let error = "";
 
   if (method === 'GET') {
     if (params.at(1) === 'inventory') {
       try{
-        const id = parseInt(params.at(2));
         let responseData ;
+        if(params.at(2)){
         
-        if(id){
+          const id = parseInt(params.at(2));
+        
+          if(id){
+            const filePath = path.join(__dirname,'public','data','inventory.json');
+            const data = await readFile(filePath);
+            const inventory = JSON.parse(data);
+            responseData = inventory.find((item)=>item.id === id);
+            if(!responseData){
+              error = JSON.stringify({error:"item not found"});
+              res.writeHead(404, {
+                'content-type': 'application/json'
+              });
+              res.write(error);
+       
+            }else{
+              res.writeHead(200, {
+                'content-type': 'application/json'
+              });
+              res.write(JSON.stringify(responseData));
+            }
+          }else{
+            error = JSON.stringify({error:"invalid input"});
+            res.writeHead(400, {
+              'content-type': 'application/json'
+            });
+            res.write(error);
+          
+          }
+          res.end();
+        }else{
           const filePath = path.join(__dirname,'public','data','inventory.json');
           const data = await readFile(filePath);
           const inventory = JSON.parse(data);
           responseData = inventory;
-          responseData = inventory.find((item)=>item.id === id);
           if(!responseData){
-            status=400;
-            error = JSON.stringify({error:"item not found"});
+            error = JSON.stringify({error:"no such file"});
+            res.writeHead(404, {
+              'content-type': 'application/json'
+            });
+            res.write(JSON.stringify(responseData));
           }else{
-            res.writeHead(status, {
+            res.writeHead(200, {
               'content-type': 'application/json'
             });
             res.write(JSON.stringify(responseData));
           }
-        }else{
-          error = JSON.stringify({error:"invalid input"});
+          res.end();
         }
-    
         
-        
-        res.end();
       }catch(error){
         res.writeHead(500, {'content-type': 'application/json'});
         res.write(JSON.stringify({error:error}));
         res.end();
       }
-      
     
     }else if(params.at(1)==="serbal.html"){
       try{
         const  filePath= path.join(__dirname,'pages','serbal.html');
         const content = await readFile(filePath);
         if(!content){
-          status=404;
+         
+          res.writeHead(404, {'content-type': 'application/json'});
           error = JSON.stringify({error:"page not found"});
           res.write(error);
 
         }else{
-          res.writeHead(status, {
+          res.writeHead(200, {
             'content-type': 'text/html'
           });
           res.write(content);
@@ -72,12 +99,15 @@ const server = http.createServer(async(req, res) => {
         const  filePath= path.join(__dirname,'pages','astronomy.html');
         const content = await readFile(filePath);
         if(!content){
-          status=404;
           error = JSON.stringify({error:"page not found"});
+          res.writeHead(404, {
+            'content-type': 'text/html'
+          });
+    
           res.write(error);
 
         }else{
-          res.writeHead(status, {
+          res.writeHead(200, {
             'content-type': 'text/html'
           });
           res.write(content);
@@ -94,12 +124,15 @@ const server = http.createServer(async(req, res) => {
         const  filePath= path.join(__dirname,'pages','home.html');
         const content = await readFile(filePath);
         if(!content){
-          status=404;
+        
           error = JSON.stringify({error:"page not found"});
+          res.writeHead(404, {
+            'content-type': 'application/json'
+          });
           res.write(error);
 
         }else{
-          res.writeHead(status, {
+          res.writeHead(200, {
             'content-type': 'text/html'
           });
           res.write(content);
@@ -111,39 +144,45 @@ const server = http.createServer(async(req, res) => {
         res.write(JSON.stringify({error:error}));
         res.end();
       }
-    }else if(params.at(1)==="index.css"){
+    }else if( params.at(2) === "home.css") {
       try{
+
         const  filePath= path.join(__dirname,'style','home.css');
         const content = await readFile(filePath);
+        
         if(!content){
-          status=404;
           error = JSON.stringify({error:"file not found"});
+          res.writeHead(404, {
+            'content-type': 'application/json'
+          });
           res.write(error);
-
         }else{
-          res.writeHead(status, {
+          
+          res.writeHead(200, {
             'content-type': 'text/css'
           });
           res.write(content);
         }
-        
         res.end();
       }catch(error){
         res.writeHead(500, {'content-type': 'application/json'});
         res.write(JSON.stringify({error:error}));
         res.end();
       }
-    }else if(params.at(1)==="astronomy.jpg"){
+    }else if(params.at(3)==="astronomy.jpg"){
       try{
         const  filePath= path.join(__dirname,'public','images','astronomy.jpg');
-        const content = await readFile(filePath);
+        const content = await readFile(filePath,null);
         if(!content){
-          status=404;
+       
           error = JSON.stringify({error:"file not found"});
+          res.writeHead(404, {
+            'content-type': 'application/json'
+          });
           res.write(error);
 
         }else{
-          res.writeHead(status, {
+          res.writeHead(200, {
             'content-type': 'image/jpeg'
           });
           res.write(content);
@@ -155,17 +194,20 @@ const server = http.createServer(async(req, res) => {
         res.write(JSON.stringify({error:error}));
         res.end();
       }
-    }else if(params.at(1)==="serbal.jpg"){
+    }else if(params.at(3)==="serbal.jpeg"){
       try{
-        const  filePath= path.join(__dirname,'public','images','serbal.jpg');
-        const content = await readFile(filePath);
+        const  filePath= path.join(__dirname,'public','images','serbal.jpeg');
+        const content = await readFile(filePath,null);
         if(!content){
-          status=404;
+        
+          res.writeHead(404, {
+            'content-type': 'application/json'
+          });
           error = JSON.stringify({error:"file not found"});
           res.write(error);
 
         }else{
-          res.writeHead(status, {
+          res.writeHead(200, {
             'content-type': 'image/jpeg'
           });
           res.write(content);
@@ -181,48 +223,70 @@ const server = http.createServer(async(req, res) => {
       try{
         const  filePath= path.join(__dirname,'pages','page-not-found.html');
         const content = await readFile(filePath);
-        if(!content){
-          status=404;
+        if(!content){   
+          res.writeHead(404, {'content-type': 'application/json'});
           error = JSON.stringify({error:"file not found"});
           res.write(error);
 
         }else{
-          res.writeHead(status, {
+          res.writeHead(200, {
             'content-type': 'text/html'
           });
           res.write(content);
         }
         
-        res.end();
+        
       }catch(error){
         res.writeHead(500, {'content-type': 'application/json'});
         res.write(JSON.stringify({error:error}));
-        res.end();
+        
       }
+      res.end();
     }
+  }else if(method === 'POST'){
+    
   }
 });
 // listenting to the server
-server.listen(3000, () => {
-  console.log('Up and running: http://127.0.0.1:3000');
+server.listen(80, () => {
+  console.log('Up and running: http://127.0.0.1');
 });
 
 /**
- * @param - takes the path and use it to get the data from the file
- * @returns - promise of the data
+ * @param {path} path- takes the path and use it to get the data from the file
+ * @param {encoding} encoding -takes the desired encoding , by default its utf-8
+ * @returns {Promise}- promise of the data
  */
-function readFile(path){
+function readFile(path,encoding = 'utf8'){
   return new Promise((resolve,reject)=>{
-    const stream = fs.createReadStream(path);
-
-    let data = '';
-    stream.on('data',(chunk)=>{
-      data+=chunk;
-    });
-    stream.on('end',()=>{
-      resolve(data);
-    });
+    const stream = fs.createReadStream(path,encoding);
     stream.on('error',reject);
+    if(encoding){
+      let data = '';
+      stream.on('data',(chunk)=>{
+        data+=chunk;
+      });
+      stream.on('end',()=>{
+        resolve(data);
+      });
+    }else{
+      const chunks = [];
+      stream.on('data',(chunk)=>{
+        chunks.push(chunk);
+      });
+      stream.on('end',()=>{
+        const data = Buffer.concat(chunks);
+        resolve(data);
+      });
+    }
+    
+  
   });
   
 }
+
+/**
+ * @param {string} content - the type of conent you wish for the res to display
+ * @param {string[]} params - the url params
+ * @param {path} filePath - path for the file
+ */
