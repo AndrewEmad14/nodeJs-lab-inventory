9593fcd89ac5e9fd56e36a0eabf9d7d09f174fb6
Andrew Emad <andrewemad@Andrews-MacBook-Pro.local>
2026-02-13 17:58:47 +0200

    feat: add basic routes from last branch except page not found

--- index.js
+++ index.js
@@ -1,266 +1,189 @@
-const http = require('http');
+//imports
+const express = require('express');
+const app = express();
 const fs = require('fs');
 const path = require('path');
+const { pipeline } = require('stream');
 
+
+//enums & constants
 const routes = {
   INVENTORY: 'inventory',
   SERBAL_HTML: 'serbal.html',
   ASTRONOMY_HTML: 'astronomy.html',
   HOME_HTML: 'home.html',
-  HOME_CSS: 'home.css',
-  ASTRONOMY_JPG: 'astronomy.jpg',
-  SERBAL_JPG: 'serbal.jpeg',
   PAGE_NOT_FOUND_HTML:'page-not-found.html'
-};
-const methods={
-  GET:'GET',
-  POST:'POST'
 };
 const types = {
   HTML_PAGE:'html',
-  CSS_PAGE:'css'
+  CSS_PAGE:'css',
+  JSON:'json'
 };
+const folders={
+  PUBLIC:'public',
+  DATA:'data',
+  IMAGES:'images',
+  STYLE:'style',
+  PAGES:'pages'
+};
+const PORT = 80;
 
-//creating a server 
-const server = http.createServer(async(req, res) => {
-  const {method, url } = req;
-  const params = url.split("/");
+// ask for  traversal attacks
+
+//uses the public folder statically
+app.use(express.static(folders.PUBLIC));
+
+// for parsing application/json requests
+app.use(express.json());          
+
+app.get(`/${routes.INVENTORY}/:inventoryId`,  (req,res)=>{
+  const filePath = path.join(__dirname,folders.PUBLIC,folders.DATA,`${routes.INVENTORY}.json`);
+  const id = req.params.inventoryId;
 
 
+  getInventoryById(filePath,res,parseInt(id));
+  
 
-  if (method === methods.GET) {
-    if (params.at(1) === routes.INVENTORY) {
-      const filePath = path.join(__dirname,'public','data',`${routes.INVENTORY}.json`);
-      if(params.at(2)){
-        const id = parseInt(params.at(2));
-        await getInventoryById(filePath,id,res);
-      } else{
-        await getInventory(filePath,res);
-      }
-    }else if(params.at(1)===routes.SERBAL_HTML){
-      const  filePath= path.join(__dirname,'pages',routes.SERBAL_HTML);
-      await getPage(filePath,res,types.HTML_PAGE);
-    }else if(params.at(1)===routes.ASTRONOMY_HTML){
-      const  filePath= path.join(__dirname,'pages',routes.ASTRONOMY_HTML);
-      await getPage(filePath,res,types.HTML_PAGE);
-    }else if(params.at(1)===routes.HOME_HTML){
-      const  filePath= path.join(__dirname,'pages',routes.HOME_HTML);
-      await getPage(filePath,res,types.HTML_PAGE);
-    }else if( params.at(2) === routes.HOME_CSS) {
-      const  filePath= path.join(__dirname,'style',routes.HOME_CSS);
-      await getPage(filePath,res,types.CSS_PAGE);
-    }else if(params.at(3)===routes.ASTRONOMY_JPG){
-      const  filePath= path.join(__dirname,'public','images',routes.ASTRONOMY_JPG);
-      await getImg(filePath,res);
-    }else if(params.at(3)===routes.SERBAL_JPG){
-      const  filePath= path.join(__dirname,'public','images',routes.SERBAL_JPG);
-      await getImg(filePath,res);
-    }else{
-      const  filePath= path.join(__dirname,'pages',routes.PAGE_NOT_FOUND_HTML);
-      await getPage(filePath,res,types.HTML_PAGE);
-    }
-  }else if(method === methods.POST){
-    if(params.at(1)=== routes.INVENTORY){
-      let body = '';
-      req.on('data',chunk =>{
-        body+= chunk.toString();
-      });
-      req.on('end',async ()=>{
-        const postData = JSON.parse(body);
-        const filePath = path.join(__dirname,'public','data',`${routes.INVENTORY}.json`);
-        await addItem(postData,filePath,res);
-      });
-    }
-  }
 });
-// listenting to the server
-server.listen(80, () => {
-  console.log('Up and running: http://127.0.0.1');
+app.get(`/${routes.INVENTORY}`,  (req,res)=>{
+  const filePath = path.join(__dirname,folders.PUBLIC,folders.DATA,`${routes.INVENTORY}.json`);
+  
+  getInventory(filePath,res);
+  
+
+});
+app.get(`/${routes.HOME_HTML}`,  (req,res)=>{
+  const filePath = path.join(__dirname,folders.PAGES,`${routes.HOME_HTML}`);
+  getPage(filePath,res);
+});
+app.get(`/${routes.ASTRONOMY_HTML}`,  (req,res)=>{
+  const filePath = path.join(__dirname,folders.PAGES,`${routes.ASTRONOMY_HTML}`);
+  getPage(filePath,res);
+});
+app.get(`/${routes.SERBAL_HTML}`,  (req,res)=>{
+  const filePath = path.join(__dirname,folders.PAGES,`${routes.SERBAL_HTML}`);
+  getPage(filePath,res);
+});
+app.get(`/${routes.PAGE_NOT_FOUND_HTML}`,  (req,res)=>{
+  const filePath = path.join(__dirname,folders.PAGES,`${routes.PAGE_NOT_FOUND_HTML}`);
+  getPage(filePath,res);
 });
 
+app.post(`/${routes.INVENTORY}`,(req,res)=>{
+  const filePath = path.join(__dirname,folders.PUBLIC,folders.DATA,`${routes.INVENTORY}.json`);
+  addItem(filePath,req,res);
+});
+
+
+
+
+
+app.listen(PORT,()=>{
+  console.log(`Example app listening on port ${PORT}`);
+});
+
 /**
- * @param {path} path- takes the path and use it to get the data from the file
- * @param {encoding} encoding -takes the desired encoding , by default its utf-8
- * @returns {Promise}- promise of the data
- */
-function readFile(path,encoding = 'utf8'){
-  return new Promise((resolve,reject)=>{
-    const stream = fs.createReadStream(path,encoding);
-    stream.on('error',reject);
-    if(encoding){
-      let data = '';
-      stream.on('data',(chunk)=>{
-        data+=chunk;
-      });
-      stream.on('end',()=>{
-        resolve(data);
-      });
-    }else{
-      const chunks = [];
-      stream.on('data',(chunk)=>{
-        chunks.push(chunk);
-      });
-      stream.on('end',()=>{
-        const data = Buffer.concat(chunks);
-        resolve(data);
-      });
-    }
-    
-  
-  });
-  
-}
-/**
- * prompts the user input
- * @param {object}postData - the data you want to add
- * @param {string} path - the path to your inventory
- * @param {Response} res -response of the server
- */
-async function addItem(postData ,path,res) {
-  try{
-    const inventoryRaw = await readFile(path);
-  
-    const inventory = JSON.parse(inventoryRaw);
-    const lastItem = inventory.at(-1);
-    const newId = (lastItem ? lastItem.id : 0  )+ 1;
-    if(!postData.itemName){
-      throw new Error("you must enter a name");
-    }
-    const itemName =  postData.itemName ;
-    if(postData.quantity<0){
-      throw new Error("cant put negative quantity");
-    }else if(postData.quantity && isNaN(Number(postData.quantity))){
-      throw new Error("quantity must be a number");
-    }
-
-    const quantity = postData.quantity ? parseInt(postData.quantity) : 1;
-    const category = postData.category ? postData.category  : "General";
-    const item = {
-      id: newId,
-      itemName: itemName,
-      quantity: quantity,
-      category: category,
-    };
-
-    const newinventory = inventory.concat(item);
-    const writeStream = fs.createWriteStream(path);
-    writeStream.write(JSON.stringify(newinventory));
-    writeStream.end();
-    res.writeHead(201,{'content-type': 'text/plain'});
-    res.write("Data created successfully");
-
-    res.end();
-
-  }catch(error){
-    res.writeHead(400,{'content-type': 'application/json'});
-    res.write(JSON.stringify({error:error.message}));
-    res.end();
-  }
-  
-  
-}
-/**
- * get all the inventory
+ * get all the inventory items
  * @param {string} path the inventory path
  * @param {Response} res the response of the server
  */
-async function getInventory(path,res){
-  const data = await readFile(path);
-  const inventory = JSON.parse(data);
-  const responseData = inventory;
-  if(!responseData){
-    const error = JSON.stringify({error:"no such file"});
-    res.writeHead(404, {
-      'content-type': 'application/json'
-    });
-    res.write(JSON.stringify(error));
-  }else{
-    res.writeHead(200, {
-      'content-type': 'application/json'
-    });
-    res.write(JSON.stringify(responseData));
-  }
-  res.end();
-}
-/**
- * get an item by an id
- * @param {string} path 
- * @param {number} id 
- * @param {Response} res 
- */
-async function getInventoryById(path,id,res){
-
-  if(id){
-
-    const data = await readFile(path);
-    const inventory = JSON.parse(data);
-    
-    const responseData = inventory.find((item)=>item.id === id);
-    if(!responseData){
-      const error = JSON.stringify({error:"item not found"});
-      res.writeHead(404, {
-        'content-type': 'application/json'
-      });
-      res.write(error);
-
-    }else{
-      res.writeHead(200, {
-        'content-type': 'application/json'
-      });
-      res.write(JSON.stringify(responseData));
+function getInventory(path,res){
+  const source =  fs.createReadStream(path);
+  res.type(types.JSON);
+  pipeline(source,res,(err) => {
+    if (err) {
+      res.set( 'Content-Type','text/plain' );
+      res.status(500).send('Streaming error');
     }
-  }else{
-    const error = JSON.stringify({error:"invalid input"});
-    res.writeHead(400, {
-      'content-type': 'application/json'
-    });
-    res.write(error);
-          
-  }
-  res.end();
+  });
+
 }
 /**
- * gets an html or a css page
+ * get an inventory item by id
+ * @param {string} path the inventory path
+ * @param {Response} res the response of the server
+ * @param {number} id target item id
+ */
+function getInventoryById(path,res,id){
+  const source =  fs.createReadStream(path);
+  let data = '';
+  source.on('data',(chunk)=>{
+    data+=chunk;
+  });
+  source.on('end',()=>{
+    const parsedData = JSON.parse(data);
+    const item = parsedData.find((item)=>item.id === id);
+    res.json(item);
+  });
+}
+
+/**
+ * gets an html page
  * @param {string} path   -path of the page
  * @param {response} res  - response object form the http server
- * @param {string} type   - decide between html or css
  */
-async function getPage(path,res,type){
-
-  const content = await readFile(path);
-  if(!content){
-    res.writeHead(404, {'content-type': 'application/json'});
-    const error = JSON.stringify({error:"page not found"});
-    res.write(error);
-
-  }else{
-    res.writeHead(200, {
-      'content-type': `text/${type}`
-    });
-    res.write(content);
-  }
-        
-  res.end();
+function getPage(path,res){
+  const source =  fs.createReadStream(path);
+  res.type(types.HTML_PAGE);
+  pipeline(source,res,(err) => {
+    if (err) {
+      res.set( 'Content-Type','text/plain' );
+      res.status(500).send('Streaming error');
+    }
+  });
+  
 }
-/**
- * get an image 
- * @param {string} path 
- * @param {response} res 
- */
-async function getImg(path,res){
-  const content = await readFile(path,null);
-  if(!content){
-    const error = JSON.stringify({error:"file not found"});
-    res.writeHead(404, {
-      'content-type': 'application/json'
-    });
-    res.write(error);
 
-  }else{
-    res.writeHead(200, {
-      'content-type': 'image/jpeg'
+/**
+ * prompts the user input
+ * @param {string} path - the path to your inventory
+ * @param {Request} req - request to the sever
+ * @param {Response} res -response of the server
+ */
+function addItem(path,req,res) {
+  try{
+    const source =  fs.createReadStream(path);
+    let data = '';
+    source.on('data',(chunk)=>{
+      data+=chunk;
     });
-    res.write(content);
+    source.on('end',()=>{
+      try{
+
+      
+        const inventory = JSON.parse(data);
+        const postData = req.body;
+        const lastItem = inventory.at(-1);
+        const newId = (lastItem ? lastItem.id : 0  )+ 1;
+        if(!postData.itemName){
+          throw new Error("you must enter a name");
+        }
+        const itemName =  postData.itemName ;
+        if(postData.quantity<0){
+          throw new Error("cant put negative quantity");
+        }else if(postData.quantity && isNaN(Number(postData.quantity))){
+          throw new Error("quantity must be a number");
+        }
+
+        const quantity = postData.quantity ? parseInt(postData.quantity) : 1;
+        const category = postData.category ? postData.category  : "General";
+        const item = {
+          id: newId,
+          itemName: itemName,
+          quantity: quantity,
+          category: category,
+        };
+
+        const newinventory = inventory.concat(item);
+        const writeStream = fs.createWriteStream(path);
+        writeStream.write(JSON.stringify(newinventory));
+        writeStream.end();
+        res.status(201).send("Item Added successfully");
+      }catch(error){
+        res.status(400).json({error:error.message});
+      }
+    });
+  }catch(error){
+    res.status(400).json({error:error.message});
   }
-  res.end();
 }
